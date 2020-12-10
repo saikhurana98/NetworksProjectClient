fe3eb5ba342790175ea688807af1ec929d17333b
unknown <cpanel.pc@gmail.com>
2020-12-10 14:41:29 +0530

    feat: mvp

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
-
+ 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
@@ -9,15 +9,20 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
+    autoHideMenuBar:true,
     width: 800,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    }
   });
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
+
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
 };
 
 // This method will be called when Electron has finished
--- src/socketClient.js
+++ src/socketClient.js
@@ -1,21 +1,50 @@
-import net from 'net';
+var net = require('net');
 
+let defaultIp = '127.0.0.1';
+let defaultPort = 42069;
+let client = new net.Socket();
 
+$(document).on('click', '.connect-btn', function() {
+    console.log("Triggered.");
+    let ip = $('[name=ip]').val();
+    let port = $('[name=port]').val();
 
+    if(!net.isIP(ip)) ip = defaultIp;
 
-// client.on('data', function (data) {
-//     console.log('Received: ' + data);
-//     client.destroy(); // kill client after server's response
-// });
+    if(port == '') { port = defaultPort; }
+    else port = parseInt(port);
 
-// client.on('close', function () {
-//     console.log('Connection closed');
-// });
-
-function connect(address = '127.0.0.1', port = 42069) {
-    var client = new net.Socket();
-    client.connect(port, address, () => {
-        console.log('Connected');
-        client.write('Hello, server! Love, Client.');
+    client.connect(port, ip, () => {
+        $('.socket-info').hide();
+        $('.dept-selection').removeClass("d-none");
     });
-}
+});
+
+$(document).on('change', '#deptsSelect', function() {
+    let selectedDeptName = $("#deptsSelect").val();
+    console.log(selectedDeptName);
+    client.write(selectedDeptName);
+});
+
+client.on('data', function (data) {
+    let obj = JSON.parse(data);
+    console.log(obj);
+    if(obj.type == 'init') {
+        for(let deptInd = 0; deptInd < obj.payload.length; deptInd++) {
+            let deptName = obj.payload[deptInd];
+            let deptOptionElem = $("<option></option>").text(deptName).val(deptName);
+            $("#deptsSelect").append(deptOptionElem);
+        }
+    }
+    else if(obj.type == 'deptOfferings') {
+        if(obj.success) {
+            $(".offerings").empty();
+            for(let offeringInd = 0; offeringInd < obj.offerings.length; offeringInd++) {
+                let offeringName = obj.offerings[offeringInd];
+                let offeringElem = $("<li></li>").text(offeringName).addClass('list-group-item');
+                $(".offerings").append(offeringElem);
+            }   
+        }
+        else console.log("Oops! Something went wrong. Here's what the server says: "+obj.msg);
+    }
+});
